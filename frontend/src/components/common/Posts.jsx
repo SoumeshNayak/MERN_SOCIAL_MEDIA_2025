1a6a829fb2148d3fe037f6c6abdc2c925e3fdd6a
Soumesh Nayak <soumeshnayak2210@gmail.com>
2025-01-24 00:22:30 +0530

    get User profile and post of user and liked post

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -4,7 +4,7 @@ import { POSTS } from "../../utils/db/dummy";
 import { useQueries, useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 
-const Posts = ({feedType}) => {
+const Posts = ({feedType,username,userId}) => {
 	
 	const getPostEndPoint=()=>{
 		switch(feedType){
@@ -12,6 +12,10 @@ const Posts = ({feedType}) => {
 				return "api/posts/allPosts"
 			case "following":
 				return "api/posts/follow"
+			case "posts":
+				return `/api/posts/user/${username}`
+			case "likes":
+				return `/api/posts/likes/${userId}`
 			default:
 				return "api/posts/allPosts"
 		}
